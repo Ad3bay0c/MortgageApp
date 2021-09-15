b04a09ccc50d09d163314bb6d7e58f9a0f2a69bf
tambarie <aclintonadebayo@gmail.com>
2021-09-15 20:36:17 +0100

    Get Bank Lists DOne and Dusted

--- bank/bank.go
+++ bank/bank.go
@@ -1,8 +1,14 @@
 package bank
 
 import (
+	"context"
+	"encoding/json"
+	"github.com/Ad3bay0c/mortgage_app/db"
 	_ "github.com/Ad3bay0c/mortgage_app/db"
+	"go.mongodb.org/mongo-driver/bson"
+	"log"
 	"net/http"
+	"time"
 )
 
 type Bank struct {
@@ -15,12 +21,42 @@ type Bank struct {
 	UpdateAt	int		`json:"update_at,omitempty" bson:"update_at,omitempty"`
 }
 
+type Message struct {
+	Message		string		`json:"message,omitempty" bson:"message,omitempty"`
+	Data		interface{}	`json:"data,omitempty" bson:"data,omitempty"`
+}
+var collection = db.Client.Database("ContactKeeper").Collection("bank")
+
 func Create(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Create Endpoint Connected"))
 }
 
 func List(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Bank List Endpoint Created"))
+	w.Header().Set("Content-Type", "application/json")
+
+	var banks []Bank
+
+	ctx, _:= context.WithTimeout(context.Background(), 10 * time.Minute)
+	result, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		log.Printf("Error: %v", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(Message{Message: "Server Error"})
+	}
+
+	for result.Next(ctx) {
+		var bank Bank
+
+		_ = result.Decode(&bank)
+		banks = append(banks, bank)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	if len(banks) == 0 {
+		json.NewEncoder(w).Encode(Message{Message: "Empty List", Data: nil})
+		return
+	}
+	json.NewEncoder(w).Encode(Message{Message: "Success", Data: banks})
 }
 
 func Update(w http.ResponseWriter, r *http.Request) {
