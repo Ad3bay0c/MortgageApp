39d673a1be04de4e59466a60744596789eb197ed
tambarie <aclintonadebayo@gmail.com>
2021-09-15 19:54:40 +0100

    Bank List Handler Created

--- bank/bank.go
+++ bank/bank.go
@@ -9,10 +9,10 @@ func Create(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Create Endpoint Connected"))
 }
 
-//func BankList(w http.ResponseWriter, r *http.Request) {
-//	w.Write([]byte("Bank List Endpoint Created"))
-//}
-//
+func List(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Bank List Endpoint Created"))
+}
+
 //func Update(w http.ResponseWriter, r *http.Request) {
 //	w.Write([]byte("Update Bank"))
 //}
