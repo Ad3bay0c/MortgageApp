13408b16756cc530b8559c2b56f9d2bdcee19a22
tambarie <aclintonadebayo@gmail.com>
2021-09-15 20:16:09 +0100

    Bank Struct Added

--- bank/bank.go
+++ bank/bank.go
@@ -5,6 +5,16 @@ import (
 	"net/http"
 )
 
+type Bank struct {
+	Name		string	`json:"name,omitempty" bson:"name,omitempty"`
+	Interest	float64	`json:"interest,omitempty" bson:"interest,omitempty"`
+	MaxLoan		float64	`json:"max_loan,omitempty" bson:"max_loan,omitempty"`
+	MinDown		float64	`json:"min_down,omitempty" bson:"min_down,omitempty"`
+	LoanTerm	float64	`json:"loan_term,omitempty" bson:"loan_term,omitempty"`
+	CreatedAt	int		`json:"created_at,omitempty" bson:"created_at,omitempty"`
+	UpdateAt	int		`json:"update_at,omitempty" bson:"update_at,omitempty"`
+}
+
 func Create(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Create Endpoint Connected"))
 }
