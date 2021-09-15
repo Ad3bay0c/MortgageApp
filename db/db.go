84c6c33a40a1d917e70e359726087e992e7b8713
tambarie <aclintonadebayo@gmail.com>
2021-09-15 19:51:47 +0100

    Database Connection Made

---
+++ db/db.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"log"
+	"time"
+)
+
+var Client *mongo.Client
+
+func init() {
+	ctx, _ := context.WithTimeout(context.Background(), 10 * time.Minute)
+	Client, _ = mongo.NewClient(options.Client().ApplyURI(URI))
+
+	err := Client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = Client.Ping(ctx, readpref.Primary())
+	if err != nil{
+		log.Fatalf("Database not connected: %v", err.Error())
+	} else {
+		log.Printf("Database Connected....")
+	}
+}
